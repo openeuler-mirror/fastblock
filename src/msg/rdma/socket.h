cfd57212079e4d53f1f18fef49af901f5fb0d89e
szy <shuzy5@chinaunicom.cn>
2024-04-02 10:03:34 +0800

    fix rdma_addrinfo leak
    Signed-off-by: szy <shuzy5@chinaunicom.cn>

--- src/msg/rdma/socket.h
+++ src/msg/rdma/socket.h
@@ -663,11 +663,6 @@ public:
     }
 
     void destroy_resource() noexcept {
-        if (_closed) [[unlikely]] {
-            SPDK_INFOLOG(msg, "the resource of the connection has been destroyed\n");
-            return;
-        }
-
         auto peer_addr = peer_address();
         auto local_addr = local_address();
 
@@ -675,8 +670,6 @@ public:
           msg,
           "destroy the resource of the socket(%s => %s)\n",
           local_addr.c_str(), peer_addr.c_str());
-
-        _closed = true;
 
         int ret;
         if (not _id) {
