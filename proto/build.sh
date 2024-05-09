aa912fb469c4aac6a6a5d3d4b2530fb4ba903b51
吴兴义 <wuxy158@chinaunicom.cn>
2024-05-09 17:00:33 +0800

    avoid unnecessary proto code generation
    Signed-off-by: 吴兴义 <wuxy158@chinaunicom.cn>

--- proto/build.sh
+++ proto/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "try to build proto files"
+
 buildtype="cpp"
 usage() {
     echo "Usage: $0 [options]"
@@ -34,11 +36,55 @@ if [ $buildtype != "cpp" ] && [ $buildtype != "golang" ];then
 fi
 
 if [ $buildtype = "cpp" ]; then
-	protoc --cpp_out=../src/rpc common_msg.proto
-	protoc --cpp_out=../src/rpc raft_msg.proto
-	protoc --cpp_out=../src/rpc osd_msg.proto
-	protoc --cpp_out=../src/monclient/ messages.proto
+    if [ ! -f ../src/rpc/common_msg.pb.h ] || [ ! -f ../src/rpc/common_msg.pb.cc ]; then
+        protoc --cpp_out=../src/rpc common_msg.proto
+    else
+        protoc --cpp_out=/tmp common_msg.proto
+        if ! cmp -s ../src/rpc/common_msg.pb.h /tmp/common_msg.pb.h || ! cmp -s ../src/rpc/common_msg.pb.cc /tmp/common_msg.pb.cc; then
+            mv /tmp/common_msg.pb.h ../src/rpc/common_msg.pb.h
+            mv /tmp/common_msg.pb.cc ../src/rpc/common_msg.pb.cc
+        fi
+    fi
+
+    if [ ! -f ../src/rpc/raft_msg.pb.h ] || [ ! -f ../src/rpc/raft_msg.pb.cc ]; then
+        protoc --cpp_out=../src/rpc raft_msg.proto
+
+    else
+        protoc --cpp_out=/tmp raft_msg.proto
+        if ! cmp -s ../src/rpc/raft_msg.pb.h /tmp/raft_msg.pb.h || ! cmp -s ../src/rpc/raft_msg.pb.cc /tmp/raft_msg.pb.cc; then
+            mv /tmp/raft_msg.pb.h ../src/rpc/raft_msg.pb.h
+            mv /tmp/raft_msg.pb.cc ../src/rpc/raft_msg.pb.cc
+        fi
+    fi
+
+
+    if [ ! -f ../src/rpc/osd_msg.pb.h ] || [ ! -f ../src/rpc/osd_msg.pb.cc ]; then
+        protoc --cpp_out=../src/rpc osd_msg.proto
+    else
+        protoc --cpp_out=/tmp osd_msg.proto
+        if ! cmp -s ../src/rpc/osd_msg.pb.h /tmp/osd_msg.pb.h || ! cmp -s ../src/rpc/osd_msg.pb.cc /tmp/osd_msg.pb.cc; then
+            mv /tmp/osd_msg.pb.h ../src/rpc/osd_msg.pb.h
+            mv /tmp/osd_msg.pb.cc ../src/rpc/osd_msg.pb.cc
+        fi
+    fi
+
+    if [ ! -f ../src/monclient/messages.pb.h ] || [ ! -f ../src/monclient/messages.pb.cc ]; then
+        protoc --cpp_out=../src/monclient messages.proto
+    else
+        protoc --cpp_out=/tmp messages.proto
+        if ! cmp -s ../src/monclient/messages.pb.h /tmp/messages.pb.h || ! cmp -s ../src/monclient/messages.pb.cc /tmp/messages.pb.cc; then
+            mv /tmp/messages.pb.h ../src/monclient/messages.pb.h
+            mv /tmp/messages.pb.cc ../src/monclient/messages.pb.cc
+        fi
+    fi
 else
-	mkdir -p ../monitor/msg
-	protoc --gogo_out=../monitor/msg/  messages.proto
+    mkdir -p ../monitor/msg
+    if [ ! -f ../monitor/msg/messages.pb.go ]; then
+        protoc --gogo_out=../monitor/msg/ messages.proto
+    else
+        protoc --gogo_out=/tmp messages.proto
+        if ! cmp -s ../monitor/msg/messages.pb.go /tmp/messages.pb.go; then
+            mv /tmp/messages.pb.go ../monitor/msg/messages.pb.go
+        fi
+    fi
 fi
