07dacae28a69b8b9c9e9d915864e371555d8c990
wangce <wangc663@chinaunicom.cn>
2026-04-28 17:10:32 +0800

    增加 block 卷能力构造 helper
    - 提供 SINGLE_NODE_WRITER block volume capability 构造函数\n- 为 smoke 联调与 gRPC 请求复用统一对象

---
+++ csi/pkg/driver/block_volume.go
@@ -0,0 +1,14 @@
+package driver
+
+import csi "github.com/container-storage-interface/spec/lib/go/csi"
+
+func SingleNodeWriterBlockVolumeCapability() *csi.VolumeCapability {
+	return &csi.VolumeCapability{
+		AccessType: &csi.VolumeCapability_Block{
+			Block: &csi.VolumeCapability_BlockVolume{},
+		},
+		AccessMode: &csi.VolumeCapability_AccessMode{
+			Mode: csi.VolumeCapability_AccessMode_SINGLE_NODE_WRITER,
+		},
+	}
+}
