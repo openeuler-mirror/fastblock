fd113cc3b356304e73eeb317f839971567df516b
wangce <wangc663@chinaunicom.cn>
2026-04-28 11:58:42 +0800

    接入 exporter 新增启动参数
    - 增加 target address 参数\n- 增加 target service id 与 NQN 前缀参数

--- exporter/cmd/fastblock-exporter/main.go
+++ exporter/cmd/fastblock-exporter/main.go
@@ -16,6 +16,9 @@ func main() {
 	flag.StringVar(&cfg.ListenAddress, "listen", cfg.ListenAddress, "exporter listen address")
 	flag.StringVar(&cfg.RPCSocketPath, "spdk-rpc-sock", cfg.RPCSocketPath, "SPDK RPC socket path")
 	flag.StringVar(&cfg.NodeName, "node-name", "", "exporter node name")
+	flag.StringVar(&cfg.TargetAddress, "target-address", "", "NVMe-oF target transport address")
+	flag.StringVar(&cfg.TargetServiceID, "target-service-id", cfg.TargetServiceID, "NVMe-oF target service id")
+	flag.StringVar(&cfg.SubsystemNQNPrefix, "nqn-prefix", cfg.SubsystemNQNPrefix, "NVMe-oF subsystem NQN prefix")
 	flag.Parse()
 
 	if err := cfg.Validate(); err != nil {
