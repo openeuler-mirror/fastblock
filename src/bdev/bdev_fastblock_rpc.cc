241317ddb06e8fc805469936ae8da575d2d1cd88
wangce <wangc663@chinaunicom.cn>
2026-05-02 19:37:28 +0800

    bdev: 增加创建卷快照RPC

--- src/bdev/bdev_fastblock_rpc.cc
+++ src/bdev/bdev_fastblock_rpc.cc
@@ -303,3 +303,63 @@ cleanup:
 }
 
 SPDK_RPC_REGISTER("bdev_fastblock_flatten", rpc_bdev_fastblock_flatten, SPDK_RPC_RUNTIME)
+
+struct rpc_bdev_fastblock_create_snapshot
+{
+	char *name;
+	char *snapshot_name;
+};
+
+static const struct spdk_json_object_decoder rpc_bdev_fastblock_create_snapshot_decoders[] = {
+	{"name", offsetof(struct rpc_bdev_fastblock_create_snapshot, name), spdk_json_decode_string},
+	{"snapshot_name", offsetof(struct rpc_bdev_fastblock_create_snapshot, snapshot_name), spdk_json_decode_string},
+};
+
+static void
+free_rpc_bdev_fastblock_create_snapshot(struct rpc_bdev_fastblock_create_snapshot *req)
+{
+	free(req->name);
+	free(req->snapshot_name);
+}
+
+static void
+rpc_bdev_fastblock_create_snapshot(struct spdk_jsonrpc_request *request,
+						  const struct spdk_json_val *params)
+{
+	struct rpc_bdev_fastblock_create_snapshot req = {};
+	struct spdk_bdev *bdev;
+	struct spdk_json_write_ctx *w;
+	int rc;
+
+	if (spdk_json_decode_object(params, rpc_bdev_fastblock_create_snapshot_decoders,
+								SPDK_COUNTOF(rpc_bdev_fastblock_create_snapshot_decoders),
+								&req))
+	{
+		spdk_jsonrpc_send_error_response(request, SPDK_JSONRPC_ERROR_INTERNAL_ERROR,
+										 "spdk_json_decode_object failed");
+		goto cleanup;
+	}
+
+	bdev = spdk_bdev_get_by_name(req.name);
+	if (bdev == NULL)
+	{
+		spdk_jsonrpc_send_error_response(request, -ENODEV, spdk_strerror(ENODEV));
+		goto cleanup;
+	}
+
+	rc = bdev_fastblock_create_snapshot(bdev, req.snapshot_name);
+	if (rc)
+	{
+		spdk_jsonrpc_send_error_response(request, rc, spdk_strerror(-rc));
+		goto cleanup;
+	}
+
+	w = spdk_jsonrpc_begin_result(request);
+	spdk_json_write_bool(w, true);
+	spdk_jsonrpc_end_result(request, w);
+
+cleanup:
+	free_rpc_bdev_fastblock_create_snapshot(&req);
+}
+
+SPDK_RPC_REGISTER("bdev_fastblock_create_snapshot", rpc_bdev_fastblock_create_snapshot, SPDK_RPC_RUNTIME)
