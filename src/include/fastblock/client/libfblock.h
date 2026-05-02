cd92aacb70972eb7660dee368c7e503a6bca7c37
wangce <wangc663@chinaunicom.cn>
2026-05-02 17:54:38 +0800

    client: 增加创建卷快照辅助接口

--- src/include/fastblock/client/libfblock.h
+++ src/include/fastblock/client/libfblock.h
@@ -89,6 +89,7 @@ public:
     void get_image_info(const std::string pool_name, const std::string image_name);
     void get_image_metadata_by_name(const std::string pool_name, const std::string image_name);
     void get_snapshot_metadata_by_id(const std::string snapshot_id);
+    void create_image_snapshot(const std::string pool_name, const std::string image_name, const std::string snapshot_name);
 
     int write(
       const uint64_t pool_id,
