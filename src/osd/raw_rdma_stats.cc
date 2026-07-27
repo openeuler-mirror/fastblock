f324d5b40465e7e1b60371ad8fc0b304288dd3f2
wuxingyi <wuxy158@chinaunicom.cn>
2026-07-27 15:41:43 +0800

    osd：实现 raw RDMA 监听端口与统计格式化
    format_raw_rdma_listen_ports/format_raw_rdma_server_stats 供启动日志复用。

---
+++ src/osd/raw_rdma_stats.cc
@@ -0,0 +1,37 @@
+/* Copyright (c) 2023-2024 ChinaUnicom
+ * fastblock is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ */
+
+#include "raw_rdma_stats.h"
+
+#include <sstream>
+
+std::string format_raw_rdma_listen_ports(const std::vector<uint16_t>& ports) {
+    std::ostringstream oss;
+    for (size_t i = 0; i < ports.size(); ++i) {
+        if (i) {
+            oss << ',';
+        }
+        oss << ports[i];
+    }
+    return oss.str();
+}
+
+std::string format_raw_rdma_server_stats(const raw_rdma_server_stats& stats) {
+    std::ostringstream oss;
+    oss << "running=" << (stats.running ? 1 : 0)
+        << " shards=" << stats.shard_count
+        << " conns=" << stats.connection_count
+        << " recv=" << stats.recv_total
+        << " send=" << stats.send_total
+        << " err=" << stats.error_total
+        << " ports=[" << format_raw_rdma_listen_ports(stats.listen_ports) << ']';
+    return oss.str();
+}
