dc9d505c8991c45f56ff32d701c097ee051c052c
yanxinyi <yanxy78@chinaunicom.cn>
2026-07-28 10:02:56 +0800

    kfastblock-admin：show-rdma-params 包含 rdma_object_log

--- kfastblock/tool/kfastblock-admin.c
+++ kfastblock/tool/kfastblock-admin.c
@@ -354,6 +354,7 @@ static int do_show_rdma_params(void)
 {
 	static const char *const names[] = {
 		"rdma_pool_enable",
+		"rdma_object_log",
 		"rdma_cm_timeout_ms",
 		"rdma_io_timeout_ms",
 		"rdma_recv_depth",
