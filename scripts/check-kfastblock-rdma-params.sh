f323919ca6183eff7b69d0085d01ea91a3eda817
yanxinyi <yanxy78@chinaunicom.cn>
2026-07-28 10:07:56 +0800

    scripts：参数检查包含 object_io_max_attempts

--- scripts/check-kfastblock-rdma-params.sh
+++ scripts/check-kfastblock-rdma-params.sh
@@ -7,6 +7,7 @@ need=(
   rdma_connect_ok rdma_pool_hit rdma_pool_miss
   rdma_pool_idle_max_age_s rdma_pool_max_idle
   rdma_object_log
+  object_io_max_attempts
 )
 missing=0
 for n in "${need[@]}"; do
