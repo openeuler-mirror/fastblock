6583f22affe506935c8ffa1f4e02b921bc84e680
wuxingyi <wuxy158@chinaunicom.cn>
2026-07-28 10:15:07 +0800

    kfastblock-admin/scripts：导出 rdma_pool_reclaim

--- scripts/check-kfastblock-rdma-params.sh
+++ scripts/check-kfastblock-rdma-params.sh
@@ -8,6 +8,7 @@ need=(
   rdma_io_timeout_total rdma_dma_map_err
   rdma_wc_err
   rdma_pool_hit rdma_pool_miss rdma_pool_evict
+  rdma_pool_reclaim
   rdma_pool_idle_max_age_s rdma_pool_max_idle
   rdma_object_log
   object_io_max_attempts
