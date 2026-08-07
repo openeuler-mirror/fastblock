c282f90dfa711bf6f1f9e2049f157781c16b3f97
Peng Yuqiang <pengyq39@chinaunicom.cn>
2026-08-07 07:49:14 +0000

    Add descriptive comments to all macros in kfastblock common.h
    Signed-off-by: Peng Yuqiang <pengyq39@chinaunicom.cn>

--- kfastblock/include/kfastblock/common.h
+++ kfastblock/include/kfastblock/common.h
@@ -3,25 +3,25 @@
 
 #include <linux/types.h>
 
-#define KFASTBLOCK_DRV_NAME "kfastblock"
-#define KFASTBLOCK_DRV_NAME_PREFIX "kfb"
-#define KFASTBLOCK_SYSFS_DEV_PREFIX "kfastblock-vol-"
+#define KFASTBLOCK_DRV_NAME "kfastblock"             /* kernel module / bus name */
+#define KFASTBLOCK_DRV_NAME_PREFIX "kfb"               /* short alias for device names */
+#define KFASTBLOCK_SYSFS_DEV_PREFIX "kfastblock-vol-"  /* sysfs device name prefix */
 
-#define KFASTBLOCK_DEVICE_PART_SHIFT 4
-#define KFASTBLOCK_DEFAULT_BLOCK_SIZE 4096U
-#define KFASTBLOCK_DEFAULT_OBJECT_SIZE (4U * 1024U * 1024U)
-#define KFASTBLOCK_DEFAULT_QUEUE_DEPTH 128U
-#define KFASTBLOCK_MAX_IO_BYTES (4U * 1024U * 1024U)
-#define KFASTBLOCK_DEFAULT_MONITOR_PORT 3334U
-#define KFASTBLOCK_DEFAULT_SOCKET_TIMEOUT_MS 3000U
-#define KFASTBLOCK_DEFAULT_TRANSPORT_MAX_ACTIVE 32U
-#define KFASTBLOCK_DEFAULT_OBJECT_DISPATCH_WINDOW 8U
-#define KFASTBLOCK_DEFAULT_REFRESH_INTERVAL_MS 3000U
-#define KFASTBLOCK_DEFAULT_IMAGE_REFRESH_INTERVAL_MS 30000U
+#define KFASTBLOCK_DEVICE_PART_SHIFT 4                 /* partition bits in minor number */
+#define KFASTBLOCK_DEFAULT_BLOCK_SIZE 4096U            /* default logical block size (bytes) */
+#define KFASTBLOCK_DEFAULT_OBJECT_SIZE (4U * 1024U * 1024U)  /* default object size (4 MiB) */
+#define KFASTBLOCK_DEFAULT_QUEUE_DEPTH 128U            /* default I/O queue depth per volume */
+#define KFASTBLOCK_MAX_IO_BYTES (4U * 1024U * 1024U)  /* maximum single I/O size (4 MiB) */
+#define KFASTBLOCK_DEFAULT_MONITOR_PORT 3334U          /* default raw monitor TCP port */
+#define KFASTBLOCK_DEFAULT_SOCKET_TIMEOUT_MS 3000U     /* socket connect/recv timeout (ms) */
+#define KFASTBLOCK_DEFAULT_TRANSPORT_MAX_ACTIVE 32U    /* max concurrent transport requests */
+#define KFASTBLOCK_DEFAULT_OBJECT_DISPATCH_WINDOW 8U   /* outstanding object dispatch slots */
+#define KFASTBLOCK_DEFAULT_REFRESH_INTERVAL_MS 3000U   /* cluster map refresh interval (ms) */
+#define KFASTBLOCK_DEFAULT_IMAGE_REFRESH_INTERVAL_MS 30000U  /* image info refresh interval (ms) */
 
-#define KFASTBLOCK_MAX_NAME_LEN 128
-#define KFASTBLOCK_MAX_ADDR_LEN 256
-#define KFASTBLOCK_LAST_ERROR_LEN 256
-#define KFASTBLOCK_MAX_MONITORS 8
+#define KFASTBLOCK_MAX_NAME_LEN 128  /* max length for image/pool names */
+#define KFASTBLOCK_MAX_ADDR_LEN 256  /* max length for network address strings */
+#define KFASTBLOCK_LAST_ERROR_LEN 256  /* buffer size for last error message */
+#define KFASTBLOCK_MAX_MONITORS 8     /* maximum number of monitor connections */
 
 #endif
