84cadf483db4a2451d44355983c12cf8f3f38ec1
Peng Yuqiang <pengyq39@chinaunicom.cn>
2026-08-07 08:10:06 +0000

    Add copyright header to top-level build and CI scripts
    Signed-off-by: Peng Yuqiang <pengyq39@chinaunicom.cn>

--- make_rpm.sh
+++ make_rpm.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
+# Copyright (c) 2023-2024 ChinaUnicom
+# fastblock is licensed under Mulan PSL v2.
+# You can use this software according to the terms and conditions of the Mulan PSL v2.
+# You may obtain a copy of Mulan PSL v2 at:
+#          http://license.coscl.org.cn/MulanPSL2
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+# EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+# MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+# See the Mulan PSL v2 for more details.
 
 rpm_version=$1
 if [[ -z $rpm_version ]]; then rpm_version="v0.1"; fi
