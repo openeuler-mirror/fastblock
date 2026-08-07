83cbf2d47d58fe39e8bf33a6d1a357f9361238e8
Peng Yuqiang <pengyq39@chinaunicom.cn>
2026-08-07 07:56:57 +0000

    Add SPDX-License-Identifier to all kfastblock kernel header files
    Signed-off-by: Peng Yuqiang <pengyq39@chinaunicom.cn>

--- kfastblock/include/kfastblock/common.h
+++ kfastblock/include/kfastblock/common.h
@@ -1,3 +1,4 @@
+/* SPDX-License-Identifier: MulanPSL-2.0 */
 #ifndef KFASTBLOCK_COMMON_H
 #define KFASTBLOCK_COMMON_H
 
--- kfastblock/include/kfastblock/meta.h
+++ kfastblock/include/kfastblock/meta.h
@@ -1,3 +1,4 @@
+/* SPDX-License-Identifier: MulanPSL-2.0 */
 #ifndef KFASTBLOCK_META_H
 #define KFASTBLOCK_META_H
 
